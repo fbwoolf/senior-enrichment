66e88f7326cb0fadbf6bf96f8ec27d39930a7acc
Fara Woolf <fbwoolf@gmail.com>
2017-07-08 09:26:10 -0500

    chore: setup db models

---
+++ db/models/student.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const db = require('../index');
+
+const Student = db.define('student', {
+	name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }
+})
+
+module.exports = Student;
